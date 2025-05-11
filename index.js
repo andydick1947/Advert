c018b4b982207bb2965d36ebeea233595a9b6484
andydick1947 <ardick2000@hotmail.com>
2025-05-11 22:13:23 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,37 +6,37 @@ for (let x=1; x<=6; x++){
 
    if(z=="sidebtn1"){ 
       clean();
-      myintro1();
+      myintro2();
       myintro();
    }
 
    if(z=="sidebtn2"){ 
       clean()
-      myloc1()
+      myloc2()
       myloc()
    }
 
    if(z=="sidebtn3"){ 
       clean()
-      mycttee1()
+      mycttee2()
       mycttee()
    }
 
    if(z=="sidebtn4"){ 
       clean()
-      mymeets1()
+      mymeets2()
       mymeets()
    }
 
    if(z=="sidebtn5"){ 
       clean()
-      myfees1()     
+      myfees2()     
       myfees()
    }
 
    if(z=="sidebtn6"){ 
       clean()
-      myres1()
+      myres2()
       myres()
    }
 
@@ -64,7 +64,13 @@ function tgle(){
       mydiv1.appendChild(div1);
       div1.innerHTML=int1;
 }
-
+   function myintro2(){
+      const int1='<p  style="color: black;font-weight:bold;font-size:30px;text-indent: 70px;">INTRODUCTION</p>';
+      const div1=document.createElement("div");
+      const mydiv1=document.getElementById("mainsect");
+      mydiv1.appendChild(div1);
+      div1.innerHTML=int1;
+}
 function myintro(){
       const int='<p id="myintro" class="mytxt"><br>The Bridge Club was founded around 1996 as the Monday Night Bridge Club, the first Director being Derek Branch. The Club continues to meet in the Llantonian Hall.  The aim of the club is to provide a regular facility for Duplicate Bridge allowing all pairs to play the same boards over the course of the evening. Ideally people attend in pairs although every attempt is made to cater for those without a regular partner.</p>';
       const div=document.createElement("div");
@@ -75,6 +81,13 @@ function myintro(){
 
 function myloc1(){
    const loc1='<p style="color: black;font-weight:bold;font-size:30px;">LOCATION<br></p>';
+   const div1=document.createElement("div");
+   const mydiv1=document.getElementById("mainsect");
+   mydiv1.appendChild(div1);
+   div1.innerHTML=loc1;
+}
+function myloc2(){
+   const loc1='<p style="color: black;font-weight:bold;font-size:30px;text-indent: 70px;">LOCATION<br></p>';
    const div1=document.createElement("div");
    const mydiv1=document.getElementById("mainsect");
    mydiv1.appendChild(div1);
@@ -100,7 +113,13 @@ const mydiv1=document.getElementById("mainsect");
 mydiv1.appendChild(div1);
 div1.innerHTML=cte1;
 }
-
+function mycttee2(){
+const cte1='<p style="color: black;font-weight:bold;font-size:30px;text-indent: 70px;">THE COMMITTEE</p>'
+const div1=document.createElement("div");
+const mydiv1=document.getElementById("mainsect");
+mydiv1.appendChild(div1);
+div1.innerHTML=cte1;
+}
 function mycttee(){
       const cte='<table id="tbl"><tr><td>Chairman/Director:</td><td>Allan Evans</td></tr><tr><td>Treasurer:</td><td>TBN</td><tr><td>Seceratary/Partnership Coordinator:</td><td>Meryl Phillips</td></tr><tr><td>Scorer:</td><td>Andy Dick</td></tr></table><br>';
       const div=document.createElement("div");
@@ -122,7 +141,13 @@ function mymeets1(){
       mydiv1.appendChild(div1);
       div1.innerHTML=mts1;
 }
-
+function mymeets2(){
+   const mts1='<p style="color: black;font-weight:bold;font-size:30px;text-indent: 70px;">MEETINGS<br></p>';
+      const div1=document.createElement("div");
+      const mydiv1=document.getElementById("mainsect");
+      mydiv1.appendChild(div1);
+      div1.innerHTML=mts1;
+}
 function mymeets(){
    const mts='<p id="meets" class="mytxt">The club meets on Monday evenings at 6.45, with play starting at 7.00 and aiming to finish by 10.00. There are also two party evenings each year</p>';
       const div=document.createElement("div");
@@ -138,6 +163,13 @@ function myfees1(){
       mydiv1.appendChild(div1);
       div1.innerHTML=fees1;
 }
+function myfees2(){
+   const fees1='<p style="color: black;font-weight:bold;font-size:30px;text-indent: 70px;">FEES<br></p>';
+      const div1=document.createElement("div");
+      const mydiv1=document.getElementById("mainsect");
+      mydiv1.appendChild(div1);
+      div1.innerHTML=fees1;
+}
 function myfees(){
    const fees='<p class="mytxt">The Fees are paid annually in January. They have been held steady at £65 for the year 2025. There is no Table charge for members, however, visitors pay £4 for each session.</p>';
       const div=document.createElement("div");
@@ -148,6 +180,13 @@ function myfees(){
 
 function myres1(){
    const res1='<p style="color: black;font-weight:bold;font-size:30px;">SCORING<br></p>';
+      const div1=document.createElement("div");
+      const mydiv1=document.getElementById("mainsect");
+      mydiv1.appendChild(div1);
+      div1.innerHTML=res1;
+}
+function myres2(){
+   const res1='<p style="color: black;font-weight:bold;font-size:30px;text-indent: 70px;">SCORING<br></p>';
       const div1=document.createElement("div");
       const mydiv1=document.getElementById("mainsect");
       mydiv1.appendChild(div1);
